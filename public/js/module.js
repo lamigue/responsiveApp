0d8d22017df31fa5ffad41f4e9aee2fc592b0ad1
lamigue <lamigue06@hotmail.fr>
2016-01-26 10:55:33 +0100

    Sign up form better style

--- public/js/module.js
+++ public/js/module.js
@@ -1,6 +1,1 @@
-var app = angular.module('wemoApp', ['ngResource', 'ngRoute', 'ngMaterial']).config(function($mdThemingProvider) {
-    // Configure a dark theme with primary foreground yellow
-    $mdThemingProvider.theme('docs-dark', 'default')
-      .primaryPalette('yellow')
-      .dark();
-});
+var app = angular.module('wemoApp', ['ngResource', 'ngRoute', 'ngMaterial']);
